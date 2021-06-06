43af57132db79f671a78b3b6e4a125b306af18c8
ks6088ts <ks6088ts@gmail.com>
2021-06-06 15:01:21 +0900

    add poetry completions

--- scripts/install-zinit.sh
+++ scripts/install-zinit.sh
@@ -12,3 +12,5 @@ soracom completion zsh | sudo tee /usr/local/share/zsh/site-functions/_soracom
 kubectl completion zsh | sudo tee /usr/local/share/zsh/site-functions/_kubectl
 # yq: https://mikefarah.gitbook.io/yq/v/v4.x/commands/shell-completion#zsh
 yq shell-completion zsh | sudo tee /usr/local/share/zsh/site-functions/_yq
+# poetry: https://python-poetry.org/docs/#enable-tab-completion-for-bash-fish-or-zsh
+poetry completions zsh | sudo tee /usr/local/share/zsh/site-functions/_poetry
