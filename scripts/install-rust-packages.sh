57f5462050929f5a4b0fc75b4a5a528bd11537ea
ks6088ts <ks6088ts@gmail.com>
2021-06-15 15:54:06 +0900

    install bottom

--- scripts/install-rust-packages.sh
+++ scripts/install-rust-packages.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+set -eu
+
+rustup component add \
+    rls \
+    rust-analysis \
+    rust-src
+
+cargo install \
+    bottom \
+    cargo-edit \
+    tealdeer
+
+# tldr
 # https://dbrgn.github.io/tealdeer/installing.html#through-cargo-install
-cargo install tealdeer
-
-tldr --update
-
 # https://dbrgn.github.io/tealdeer/installing.html#autocompletion
+tldr --update
 curl https://raw.githubusercontent.com/dbrgn/tealdeer/master/zsh_tealdeer --output /usr/local/share/zsh/site-functions/_tldr
--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -5,12 +5,4 @@ set -eu
 # https://www.rust-lang.org/tools/install
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-source $HOME/.cargo/env
-
-rustup component add \
-    rls \
-    rust-analysis \
-    rust-src
-
-cargo install \
-    cargo-edit
+# source $HOME/.cargo/env
